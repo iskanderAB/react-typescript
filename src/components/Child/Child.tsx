6285aeb92d0758acc3d74aed1a0252234cde7c1c
iskanderAB <iskanderabasi@gmail.com>
2021-09-22 21:51:25 +0100

    basics typescript

--- src/components/Child/Child.tsx
+++ src/components/Child/Child.tsx
@@ -1,10 +1,27 @@
-interface parentProps  {
-    color :string
+import React, { Fragment } from "react";
+
+interface parentProps {
+  color: string;
 }
-export const Child = ({color} : parentProps) => {
-    return(
-        <div style={{color:color}}>
-            hello
-        </div>
-    )
-}
+export const Child = ({ color }: parentProps) => {
+    const onChange = (e:React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
+    }
+
+    const onDrag = (e :React.DragEvent<HTMLDivElement>) => {
+        console.log(e.clientX , e.clientY);
+        console.log(e) ;
+    } 
+
+  return (
+    <Fragment>
+      <div style={{ color: color }}>hello</div>
+      <input onChange={onChange} />
+      <div 
+        style={{width:'50vmin',height:'50vmin',background : '#f00'}}
+        draggable 
+        onDrag={onDrag}
+        > </div>
+    </Fragment>
+  );
+};
